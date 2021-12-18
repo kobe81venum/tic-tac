c2c8045f8c42c206ce7348bdf685f863dd0a88f9
kobe81venum <54062926+kobe81venum@users.noreply.github.com>
2021-12-18 11:17:38 +0100

    pole

--- tic-tac.py
+++ tic-tac.py
@@ -14,10 +14,72 @@ for key in plansza_do_gry:
 # print(klawisze_gry)
 
 def drukuj_plansze(pole):
-    print(f"{pole['7']}|{pole['8']}|{pole['9']}")
-    print(f"{pole['4']}|{pole['5']}|{pole['6']}")
-    print(f"{pole['1']}|{pole['2']}|{pole['3']}")
+    print(f"{pole['7']} | {pole['8']} | {pole['9']}")
+    print('- + - + -')
+    print(f"{pole['4']} | {pole['5']} | {pole['6']}")
+    print('- + - + -')
+    print(f"{pole['1']} | {pole['2']} | {pole['3']}")
 
-drukuj_plansze()
+# drukuj_plansze(plansza_do_gry)
 
+def gra():
+
+    gracz = 'X'
+    licznik=0
+
+    for i in range(10):
+        drukuj_plansze(plansza_do_gry)
+
+        move=input(f"To jest ruch, {gracz}. Wybierz gdzie chcesz postawić znak")
+
+        if plansza_do_gry[move] == ' ':
+            plansza_do_gry[move] = gracz
+            licznik += 1
+        else:
+            print('miejsce zajęte\nwstaw znak w inne pole')
+            continue
+
+        if licznik >=5:  #i
+            if plansza_do_gry['7'] == plansza_do_gry['8'] == plansza_do_gry['9'] != ' ':
+                drukuj_plansze(plansza_do_gry)
+                print("\nKoniec Gry")
+                print(f"Wygrał gracz: {gracz}")
+                break
+            elif plansza_do_gry['4'] == plansza_do_gry['5'] == plansza_do_gry['6'] != ' ':
+                drukuj_plansze(plansza_do_gry)
+                print("\nKoniec Gry")
+                print(f"Wygrał gracz: {gracz}")
+                break
+            elif plansza_do_gry['1'] == plansza_do_gry['2'] == plansza_do_gry['3'] != ' ':
+                drukuj_plansze(plansza_do_gry)
+                print("\nKoniec Gry")
+                print(f"Wygrał gracz: {gracz}")
+                break
+            elif plansza_do_gry['1'] == plansza_do_gry['4'] == plansza_do_gry['7'] != ' ':
+                drukuj_plansze(plansza_do_gry)
+                print("\nKoniec Gry")
+                print(f"Wygrał gracz: {gracz}")
+                break
+            elif plansza_do_gry['2'] == plansza_do_gry['5'] == plansza_do_gry['8'] != ' ':
+                drukuj_plansze(plansza_do_gry)
+                print("\nKoniec Gry")
+                print(f"Wygrał gracz: {gracz}")
+                break
+            elif plansza_do_gry['3'] == plansza_do_gry['6'] == plansza_do_gry['9'] != ' ':
+                drukuj_plansze(plansza_do_gry)
+                print("\nKoniec Gry")
+                print(f"Wygrał gracz: {gracz}")
+                break
+            elif plansza_do_gry['1'] == plansza_do_gry['5'] == plansza_do_gry['9'] != ' ':
+                drukuj_plansze(plansza_do_gry)
+                print("\nKoniec Gry")
+                print(f"Wygrał gracz: {gracz}")
+                break
+            elif plansza_do_gry['3'] == plansza_do_gry['5'] == plansza_do_gry['7'] != ' ':
+                drukuj_plansze(plansza_do_gry)
+                print("\nKoniec Gry")
+                print(f"Wygrał gracz: {gracz}")
+                break
+            else:
+                print("remis")
 # %%
