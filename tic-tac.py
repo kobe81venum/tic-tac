c4420a48744aba21e8e3c0573b158239e8a68e5e
kobe81venum <m.sybila@gmail.com>
2021-12-18 10:25:29 +0100

    pole

--- tic-tac.py
+++ tic-tac.py
@@ -11,16 +11,13 @@ klawisze_gry=[]
 for key in plansza_do_gry:
     klawisze_gry.append(key)
 
-print(klawisze_gry)
+# print(klawisze_gry)
 
-def drukuj_plansze():
-    print(' ' + '|' + ' ' + '|')
-    print('-+-+-')
-    print(' ' + '|' + ' ' + '|')
-    print('-+-+-')
-    print(' ' + '|' + ' ' + '|')
-    print('-+-+-')
-    
+def drukuj_plansze(pole):
+    print(f"{pole['7']}|{pole['8']}|{pole['9']}")
+    print(f"{pole['4']}|{pole['5']}|{pole['6']}")
+    print(f"{pole['1']}|{pole['2']}|{pole['3']}")
+
 drukuj_plansze()
 
 # %%
