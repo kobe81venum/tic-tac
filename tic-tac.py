9510755cacb37a0be56eed2c6a05c27359271dfe
kobe81venum <m.sybila@gmail.com>
2021-12-18 10:13:52 +0100

    13

--- tic-tac.py
+++ tic-tac.py
@@ -1,3 +1,26 @@
 #%%
 #main game section
 
+# %%
+plansza_do_gry = {'7':' ','8':' ','9':' ',
+                  '4':' ','5':' ','6':' ',
+                  '1':' ','2':' ','3':' '}
+
+klawisze_gry=[]
+
+for key in plansza_do_gry:
+    klawisze_gry.append(key)
+
+print(klawisze_gry)
+
+def drukuj_plansze():
+    print(' ' + '|' + ' ' + '|')
+    print('-+-+-')
+    print(' ' + '|' + ' ' + '|')
+    print('-+-+-')
+    print(' ' + '|' + ' ' + '|')
+    print('-+-+-')
+    
+drukuj_plansze()
+
+# %%
